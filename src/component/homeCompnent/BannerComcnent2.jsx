397c54f4b78e8ce8510a81a3fa6e6d6eb75fab33
PRONOB BISWAS <80696443+pronobiswas@users.noreply.github.com>
2025-04-28 13:09:22 +0600

    update

--- src/component/homeCompnent/BannerComcnent2.jsx
+++ src/component/homeCompnent/BannerComcnent2.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { BsBoxArrowUpRight } from "react-icons/bs";
+import { div } from "three/tsl";
 
 const BannerComcnent2 = () => {
   const textRef = useRef(null);
@@ -14,34 +15,58 @@ const BannerComcnent2 = () => {
     tl.to(textRef.current, {
       y: 0,
       duration: 1,
-      opacity: 1, 
-      ease: "power2.out",
-    })
-    tl.to(scheduleRef.current, { 
-      width: "170px",
-      duration: 1,
-      opacity: 1, 
+      opacity: 1,
       ease: "power2.out",
     });
-    tl.to(boxRef1.current, { 
-      x: 0,
+    tl.to(scheduleRef.current, {
+      width: "170px",
       duration: 1,
-      opacity: 1, 
+      opacity: 1,
       ease: "power2.out",
-    },"<");
-    tl.to(boxRef2.current, { 
-      y: 0,
-      duration: 1,
-      opacity: 1, 
-      ease: "power2.out",
-    },"<")
-    tl.to(boxRef3.current, { 
-      x: 0,
-      duration: 1,
-      opacity: 1, 
-      ease: "power2.out",
-    },"<");
-    
+    });
+    tl.to(
+      boxRef1.current,
+      {
+        x: 0,
+        duration: 1,
+        opacity: 1,
+        ease: "power2.out",
+      },
+      "<"
+    );
+    tl.to(
+      boxRef2.current,
+      {
+        y: 0,
+        duration: 1,
+        opacity: 1,
+        ease: "power2.out",
+      },
+      "<"
+    );
+    tl.to(
+      boxRef3.current,
+      {
+        x: 0,
+        duration: 1,
+        opacity: 1,
+        ease: "power2.out",
+      },
+      "<"
+    );
+
+    gsap.to(".colorBox", {
+      scale: 1.2,
+      duration: 0.5,
+      repeat: -1,
+      yoyo: true,
+      stagger: {
+        amount: 0.5,
+        from: "center",
+        grid: "auto",
+        ease: "power1.inOut",
+      },
+    });
   }, []);
   return (
     <>
@@ -70,23 +95,55 @@ const BannerComcnent2 = () => {
           </nav>
         </header>
         <div className="py-15">
-
           <div className="container flex justify-center items-center mx-auto mb-20">
-            <div className="banner_img w-30 h-30 border-2 border-[#797979] rounded-xl">
-              <img src="#" alt="" />
+            <div className="banner_img w-40 h-30 rounded-xl grid grid-cols-10 gap-1">
+              {
+                new Array(70).fill(0).map((_, index) => {
+                  const colors = [
+                    "bg-red-500",
+                    "bg-green-500",
+                    "bg-blue-500",
+                    "bg-yellow-500",
+                    "bg-pink-500",
+                    "bg-purple-500",
+                    "bg-indigo-500",
+                    "bg-teal-500",
+                    "bg-orange-500",
+                    "bg-rose-500",
+                  ];
+
+                  const randomColor = colors[index % colors.length];
+
+                  return (
+                    <div 
+                      key={index} 
+                      className={`colorBox w-full h-full ${randomColor} inline-block scale-1`}
+                    ></div>
+                  );
+                })
+              }
+
             </div>
           </div>
 
           <div className="banner_container container mx-auto">
             <div className="banner_text text-white mb-15">
               <div className="text-center w-full ">
-                <h1 ref={textRef} className="text-6xl text-center translate-y-40 opacity-0">Verify to Trust Ai</h1>
+                <h1
+                  ref={textRef}
+                  className="text-6xl text-center translate-y-40 opacity-0"
+                >
+                  Verify to Trust Ai
+                </h1>
               </div>
               <p className="w-full max-w-[320px] text-center mx-auto mt-5 mb-8">
                 Transforming Ideas into Reality with Cutting-Edge Technology
               </p>
               {/* ==schedule== */}
-              <div ref={scheduleRef} className="slide_box mx-auto w-[44px] flex items-center gap-2 p-[2px] bg-white rounded cursor-pointer">
+              <div
+                ref={scheduleRef}
+                className="slide_box mx-auto w-[44px] flex items-center gap-2 p-[2px] bg-white rounded cursor-pointer"
+              >
                 <div className="slide_icon w-10 h-10 bg-green-700 flex items-center justify-center rounded shrink-0">
                   {" "}
                   &gt;
@@ -94,11 +151,14 @@ const BannerComcnent2 = () => {
                 <span className="text-black text-nowrap">Schedule Demo</span>
               </div>
             </div>
-            
+
             <div>
               <p className="text-center text-white mb-4">Lorem, ipsum dolor.</p>
               <div className="text-white flex justify-center gap-4">
-                <div ref={boxRef1} className="py-4 px-12 flex flex-col items-center bg-[#4e4e4e62] rounded-2xl translate-x-[-200px] opacity-0">
+                <div
+                  ref={boxRef1}
+                  className="py-4 px-12 flex flex-col items-center bg-[#4e4e4e62] rounded-2xl translate-x-[-200px] opacity-0"
+                >
                   <div className="icon_box bg-[#4e4e4e62] p-5 rounded">
                     <span className="text-4xl">
                       <BsBoxArrowUpRight />
@@ -107,7 +167,10 @@ const BannerComcnent2 = () => {
                   <span>Dell</span>
                   <p>Appliance</p>
                 </div>
-                <div ref={boxRef2} className="py-4 px-12 flex flex-col items-center bg-[#4e4e4e62] rounded-2xl translate-y-[200px] opacity-0">
+                <div
+                  ref={boxRef2}
+                  className="py-4 px-12 flex flex-col items-center bg-[#4e4e4e62] rounded-2xl translate-y-[200px] opacity-0"
+                >
                   <div className="icon_box bg-[#4e4e4e62] p-5 rounded">
                     <span className="text-4xl">
                       <BsBoxArrowUpRight />
@@ -116,7 +179,10 @@ const BannerComcnent2 = () => {
                   <span>Dell</span>
                   <p>Appliance</p>
                 </div>
-                <div ref={boxRef3} className="py-4 px-12 flex flex-col items-center bg-[#4e4e4e62] rounded-2xl translate-x-[200px] opacity-0">
+                <div
+                  ref={boxRef3}
+                  className="py-4 px-12 flex flex-col items-center bg-[#4e4e4e62] rounded-2xl translate-x-[200px] opacity-0"
+                >
                   <div className="icon_box bg-[#4e4e4e62] p-5 rounded">
                     <span className="text-4xl">
                       <BsBoxArrowUpRight />
@@ -128,7 +194,6 @@ const BannerComcnent2 = () => {
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </>
